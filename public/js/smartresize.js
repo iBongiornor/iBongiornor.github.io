a6109b48e4c164c5ed66ac032f314148f64d8663
Bongiornor <bongiornor@126.com>
2018-08-15 08:12:55 +0800

    delete public in hexo branch

--- public/js/smartresize.js
+++
@@ -1,1 +0,0 @@
-+function(i,e){var n=function(i,e,n){var r;return function t(){var u=this,f=arguments;function s(){if(!n){i.apply(u,f)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(u,f)}r=setTimeout(s,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
